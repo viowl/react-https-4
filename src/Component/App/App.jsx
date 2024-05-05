92961f03608981b70526a4c2515ee639ccd862ac
viowl <sovaworkbox@gmail.com>
2024-05-05 22:09:42 +0200

    add handleLoadMore button

--- src/Component/App/App.jsx
+++ src/Component/App/App.jsx
@@ -6,24 +6,36 @@ import { fetchArticles } from "../../article-ap";
 import Loader from "../Loader/Loader";
 
 export default function App() {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const handleSearch = async (newQuery) => {
-    try {
-      setIsLoading(true);
-      setArticles([]);
-      setError(false);
-      const data = await fetchArticles(newQuery);
-      setArticles(data.results);
-    } catch (e) {
-      setError(true);
-    } finally {
-      setIsLoading(false);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const data = await fetchArticles(query, page);
+        setArticles(data.results);
+        setError(false);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
+    getData();
+  }, [query, page]);
+
+  const handleSearch = async (newQuery) => {
+    setQuery(newQuery);
     console.log(newQuery);
   };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
@@ -31,7 +43,9 @@ export default function App() {
       {error && <p className={css.p}>Oops! Error! Reload! </p>}
 
       {articles.length > 0 && <ImageGallery items={articles} />}
-      {articles.length > 0 && <button>Load more</button>}
+      {articles.length > 0 && (
+        <button onClick={handleLoadMore}>Load more</button>
+      )}
     </div>
   );
 }
--- src/article-ap.js
+++ src/article-ap.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 
-export const fetchArticles = async (query) => {
+export const fetchArticles = async (query, page) => {
   const response = await axios.get("/search/photos", {
     params: {
       query: query,
       per_page: 10,
       client_id: "JPaWKYjenJwTJReunNYKAxgGxmQ5PDb5eOvxhAmCdvk",
+      page,
     },
   });
   console.log(response);
